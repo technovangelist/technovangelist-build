8a148f942a2b279e4271863054c5990bb05c407f
technovangelist <mattw@datadoghq.com>
2018-01-15 15:13:59 -0500

    more

--- gbuild.tf
+++ gbuild.tf
@@ -52,7 +52,7 @@ resource "aws_iam_policy_attachment" "codebuild_policy_attachment" {
 resource "aws_codebuild_project" "buildtechnovangelistbuilder" {
     name = "buildtechnovangelistbuilder"
     description = "CodeBuild project to build technovangelist.com"
-    build_timeout= "5"
+    build_timeout= "6"
     service_role="${aws_iam_role.codebuild_role.arn}"
 
     artifacts {
@@ -63,12 +63,16 @@ resource "aws_codebuild_project" "buildtechnovangelistbuilder" {
         compute_type = "BUILD_GENERAL1_SMALL"
         image = "jch254/dind-terraform-aws"
         type = "LINUX_CONTAINER"
+        privileged_mode = true     # don't set this and you get errors on the install
     }
 
     source {
         type = "GITHUB"
-        location = "https://github.com/technovangelist/technovangelist-build"
+        location = "https://github.com/technovangelist/technovangelist-build.git"
         buildspec = "builder-buildspec.yml"
+    }
+    provisioner "local-exec" {
+        command = "aws codebuild create-webhook --project-name buildtechnovangelistbuilder"
     }
 
 }
