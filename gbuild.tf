bd351fdc0631a622cfdb79073a0484116b34be03
technovangelist <mattw@datadoghq.com>
2018-01-15 15:16:38 -0500

    update changes to policy

--- gbuild.tf
+++ gbuild.tf
@@ -35,7 +35,8 @@ resource "aws_iam_policy" "codebuild_policy" {
             "Action": [
                 "logs:CreateLogGroup", 
                 "logs:CreateLogStream", 
-                "logs:PutLogEvents"
+                "logs:PutLogEvents", 
+                "ecr:GetAuthorizationToken"
             ]
         }
     ]
@@ -52,7 +53,7 @@ resource "aws_iam_policy_attachment" "codebuild_policy_attachment" {
 resource "aws_codebuild_project" "buildtechnovangelistbuilder" {
     name = "buildtechnovangelistbuilder"
     description = "CodeBuild project to build technovangelist.com"
-    build_timeout= "6"
+    build_timeout= "5"
     service_role="${aws_iam_role.codebuild_role.arn}"
 
     artifacts {
