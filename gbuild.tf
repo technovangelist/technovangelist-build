13a63ff5b3a45fb9a172bba0156e4bdcae8ef3bd
technovangelist <mattw@datadoghq.com>
2018-01-15 14:13:11 -0500

    first commit of build repo

---
+++ gbuild.tf
@@ -0,0 +1,73 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_iam_role" "codebuild_role" {
+    name = "codebuild-role"
+    assume_role_policy = <<ENDPOLICY
+{
+    "Version": "2012-10-17", 
+    "Statement": [
+        {
+            "Effect": "Allow", 
+            "Principal": {
+                "Service": "codebuild.amazonaws.com"
+            }, 
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+ENDPOLICY
+}
+
+resource "aws_iam_policy" "codebuild_policy" {
+    name = "codebuild-policy"
+    path = "/service-role/"
+    policy = <<ENDPOLICY
+{
+    "Version": "2012-10-17", 
+    "Statement": [
+        {
+            "Effect": "Allow", 
+            "Resource": [
+                "*"
+            ], 
+            "Action": [
+                "logs:CreateLogGroup", 
+                "logs:CreateLogStream", 
+                "logs:PutLogEvents"
+            ]
+        }
+    ]
+}
+ENDPOLICY
+}
+
+resource "aws_iam_policy_attachment" "codebuild_policy_attachment" {
+    name = "codebuild-policy-attachment"
+    policy_arn ="${aws_iam_policy.codebuild_policy.arn}"
+    roles = ["${aws_iam_role.codebuild_role.id}"]
+}
+
+resource "aws_codebuild_project" "buildtechnovangelistbuilder" {
+    name = "buildtechnovangelistbuilder"
+    description = "CodeBuild project to build technovangelist.com"
+    build_timeout= "5"
+    service_role="${aws_iam_role.codebuild_role.arn}"
+
+    artifacts {
+        type = "NO_ARTIFACTS"
+    }
+
+    environment {
+        compute_type = "BUILD_GENERAL1_SMALL"
+        image = 
+        type = "LINUX_CONTAINER"
+    }
+
+    source {
+        type = "GITHUB"
+        location = ""
+    }
+    
+}
